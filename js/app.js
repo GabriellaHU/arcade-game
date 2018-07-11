79f5ed0dff2360d3d524aae30875c4fe4c84a2c0
Gabriella-Karacsonyi <gabi.karacsonyi@gmail.com>
2018-07-12 00:06:16 +0200

    write pseudo code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+const Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+
+    //pos-x
+    //pos-y
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +17,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // check enemy position
+       //if enemy didn't pass border
+         // move forward
+         // increment x by speed * dt
+
+       //else reset position to start
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -26,20 +37,41 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 class Player {
 
+    //Constructor
+
+    //Properties
+      // x position
+      // y position
+      // sprite
+
+    //Methods
     update() {
+      //update position
+        //check collision (player x and y matches enemy coordinates)
+        //check win condition (player y matches the right y coordinates )
     }
 
     handleInput(keyCode) {
+      //update player coordinates based on keyboard input
     }
 
     render() {
+      //draw player on current x and y coordinates
     }
+
+    //Reset player
+      // set x and y to the start coordinates
 
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+// new Player object
+
+// init allEnemies Array
+// create new Enemy objects and push them to the array
 
 
 
